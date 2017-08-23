4b4a56930ba6d9796e1fc78c3c08c900e6c52f85
dalton <dalton.polhill@tulip.io>
2017-08-23 15:58:22 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php include("includes/header.html"); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Portfolio Item Heading -->
+        <div class="row">
+            <div class="span4">
+                <img style="float:right; margin-left: 75px" class="img-responsive" src="images/me.jpg"/>
+                <div class="content-heading "><h3>Dalton Polhill - Co-op Work Term Report </h3></div>
+                <p>The University of Guelph's Computer Science department offers an amazing cooperative education program that I am glad to be apart of.
+                    This is a website that I built as a report of my experiences in the co-op program, describing my experiences
+                    at the various employers throughout my co-op terms.
+                </p>
+                <div style="clear:both"></div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Related Projects Row -->
+        <div class="row">
+
+            <div class="col-lg-12">
+                <h3 class="page-header">Co-op Positions</h3>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="tulip_about.php">
+                    <h3 style="margin-top: -10px"><small>Term #1 - Summer 2017</small></h3>
+                    <img class="img-responsive portfolio-item" src="images/tulip-retail.png" alt="">
+                </a>
+            </div>
+
+            <!-- save for future terms
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <h3 style="margin-top: -10px"><small>Term #2</small></h3>
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <h3 style="margin-top: -10px"><small>Term #3</small></h3>
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <h3 style="margin-top: -10px"><small>Term #4</small></h3>
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <h3 style="margin-top: -10px"><small>Term #5</small></h3>
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            -->
+
+        </div>
+        <!-- /.row -->
+
+    </div>
+
+    <?php include("includes/footer.html"); ?>
